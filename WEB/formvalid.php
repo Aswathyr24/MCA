3bd3ccd4e0d060fc9f08503bff05dd96d0d0f378
Aswathyr24 <aswathyremesh4@gmail.com>
2025-12-03 14:20:52 +0530

    Add files via upload

--- WEB/formvalid.php
+++ WEB/formvalid.php
@@ -1,110 +1,86 @@
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Validation</title>
+	<title>FORM VALIDATION</title>
 </head>
-<style>
-    table{
-        background-color: white;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top:1em;
-        padding:1em;
-        box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
-    }
-    tr,td,th{
-        padding:1em;
-        text-align: left;
-    }
-    .center th{
-        text-align: center;
-    }
-    h2{
-        text-align: center;
-        margin-top: 2em;
-        background-color:black;
-        color: white;
-    }
-</style>
 <body>
-    <h2>FORM VALIDATION</h2>
-    <form name="form" action="" method="POST" onsubmit="return validateForm()">
-        <table>
-            <tr>
-                <th>Name</th>
-                <td><input type="text" name="fname"> </td>
-            </tr>
-            <tr>
-                <th>Email </th>
-                <td><input type="email" name="email"> </td>
-            </tr>
-            <tr>
-                <th>Mob NO </th>
-                <td><input type="tel" name="mob"> </td>
-            </tr>
-            <tr>
-                <th>Username </th>
-                <td><input type="text" name="user"> </td>
-            </tr>
-            <tr>
-                <th>Password </th>
-                <td><input type="password" name="passcode"> </td>
-            </tr>
-            <tr class="center">
-                <th colspan="2"><input type="submit" value="submit"></th>
-            </tr>
-        </table>
-    </form>
+	<h2 align="center">FORM VALIDATION</h2>
+	<form method="POST" action="#">
+		<table border="1" align="center" cellpadding="10">
+			<tr>
+				<th>Name</th>
+				<td><input type="text" name="fname"></td>
+			</tr>
+			<tr>
+				<th>Email</th>
+				<td><input type="email" name="email"></td>
+			</tr>
+			<tr>
+				<th>Mobile</th>
+				<td><input type="text" name="mob"></td>
+			</tr>
+			<tr>
+				<th>Username</th>
+				<td><input type="text" name="user"></td>
+			</tr>
+			<tr>
+				<th>Password</th>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<th colspan="2"><input type="submit" name="submit" value="Submit">
+				</th>
+			</tr>
+		</table>
+	</form>
+<?php
+$con = mysqli_connect("localhost","root","","cek");
 
-    <script>
-        function validateForm() {
-            var fname = document.form.fname.value;
-            var mob = document.form.mob.value;
-            var user = document.form.user.value;
-            var passcode = document.form.passcode.value;
-            var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-            var ren = /[0-9!@#$%^&*]/
-
-            if (fname == "") {
-                alert("Enter First Name !!");
-                document.form.fname.focus();
-                return false;
-            }
-            else if(ren.test(fname)){
-                alert("Enter Valid Name !!");
-                document.form.fname.focus();
-                return false;
-            }
-
-            if (mob == "") {
-                alert("Enter Mobile number");
-                return false;
-            }
-            else if (isNaN(mob)) {
-                alert("Enter valid Mobile number");
-                return false;
-            }
-            else if (mob.length != 10) {
-                alert("Enter Mobile number with 10 digit");
-                return false;
-            }
-
-            if (user == "") {
-                alert("Enter User Name !!");
-                document.form.user.focus();
-                return false;
-            }
-
-            if (passcode == "" || passcode.length < 8 || !re.test(passcode)) {
-                alert("Enter minimum 8 digit Password !!" + '\n' + "Atleast one Lowercase & uppercase & digti & special charecter must be included");
-                document.form.passcode.focus();
-                return false;
-            }
-
-        }
-    </script>
+if(!$con)
+{
+	die("Connection failed:".mysqli_connect_error());
+}
+if(isset($_POST['submit']))
+{
+	$name = $_POST['fname'];
+	$email = $_POST['email'];
+	$mob = $_POST['mob'];
+	$user = $_POST['user'];
+	$password = $_POST['password'];
+	
+	if($name == "" || $email == "" || $mob == "" ||$user == "" || $password == "")
+	{
+		echo "<script>alert('Please fill all fields!');</script>";
+	}
+	elseif(!preg_match("/^[a-zA-Z]*$/",$name))
+	{
+		echo "<script>alert('Enter a valid name!');</script>";
+	}
+	elseif(!filter_var($email,FILTER_VALIDATE_EMAIL))
+	{
+		echo "<script>alert('Enter a valid email address!');</script>";
+	}
+	elseif(!preg_match('/^[0-9]{10}$/',$mob))
+	{
+		echo "<script>alert('Enter a valid 10-digit mobile number!');</script>";
+	}
+	elseif(strlen($password)<8)
+	{
+		echo "<script>alert('Password must have at least 8 chartacters!');</script>";
+	}
+	else
+	{
+		$query = "INSERT INTO form(name,email,mob,password,user) VALUES ('$name','$email','$mob','$password','$user')";
+		if(mysqli_query($con,$query))
+		{
+			echo "<script>alert('Record inserted successfully!');</script>";
+		}
+		else
+		{
+			echo "<script>alert('Error inserting record!');</script>";
+		}
+	}
+}
+mysqli_close($con);
+?>
 </body>
-
 </html>
